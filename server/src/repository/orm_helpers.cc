a365db38da20e9cb5331672000f879322e1c0eca
hzh-betty <beidi_99@outlook.com>
2026-05-22 21:40:49 +0800

    feat(repository): ORM 映射和数据库访问层

---
+++ server/src/repository/orm_helpers.cc
@@ -0,0 +1,68 @@
+#include "repository/orm_helpers.h"
+
+#include <utility>
+
+namespace zchat {
+
+OrmRepositoryBase::OrmRepositoryBase(std::shared_ptr<drogon::orm::DbClient> db)
+    : db_(std::move(db)) {}
+
+std::string FieldString(const drogon::orm::Row &row, const char *name) {
+    if (row[name].isNull()) {
+        return std::string();
+    }
+    return row[name].as<std::string>();
+}
+
+std::int64_t FieldInt64(const drogon::orm::Row &row, const char *name) {
+    if (row[name].isNull()) {
+        return 0;
+    }
+    return row[name].as<std::int64_t>();
+}
+
+UserRecord ToUserRecord(const drogon::orm::Row &row) {
+    UserRecord user;
+    user.user_id = FieldString(row, "user_id");
+    user.nickname = FieldString(row, "nickname");
+    user.description = FieldString(row, "description");
+    user.password = FieldString(row, "password");
+    user.phone = FieldString(row, "phone");
+    user.avatar_id = FieldString(row, "avatar_id");
+    return user;
+}
+
+MessageRecord ToMessageRecord(const drogon::orm::Row &row) {
+    MessageRecord message;
+    message.message_id = FieldString(row, "message_id");
+    message.session_id = FieldString(row, "session_id");
+    message.user_id = FieldString(row, "user_id");
+    message.message_type = static_cast<int>(FieldInt64(row, "message_type"));
+    message.create_time = FieldInt64(row, "create_time");
+    message.content = FieldString(row, "content");
+    message.file_id = FieldString(row, "file_id");
+    message.file_name = FieldString(row, "file_name");
+    message.file_size =
+        static_cast<std::uint64_t>(FieldInt64(row, "file_size"));
+    return message;
+}
+
+ChatSessionRecord ToChatSessionRecord(const drogon::orm::Row &row) {
+    ChatSessionRecord session;
+    session.chat_session_id = FieldString(row, "chat_session_id");
+    session.chat_session_name = FieldString(row, "chat_session_name");
+    session.chat_session_type =
+        static_cast<ChatSessionType>(FieldInt64(row, "chat_session_type"));
+    return session;
+}
+
+FileRecord ToFileRecord(const drogon::orm::Row &row) {
+    FileRecord file;
+    file.file_id = FieldString(row, "file_id");
+    file.file_name = FieldString(row, "file_name");
+    file.file_size = static_cast<std::uint64_t>(FieldInt64(row, "file_size"));
+    file.file_content = FieldString(row, "file_content");
+    return file;
+}
+
+} // namespace zchat
---
+++ server/src/repository/orm_helpers.h
@@ -0,0 +1,58 @@
+#ifndef ZCHAT_SERVER_SRC_REPOSITORY_ORM_HELPERS_H_
+#define ZCHAT_SERVER_SRC_REPOSITORY_ORM_HELPERS_H_
+
+#include "common/noncopyable.h"
+
+#include <cstdint>
+#include <exception>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include <drogon/orm/DbClient.h>
+#include <drogon/orm/Exception.h>
+#include <drogon/orm/Row.h>
+
+#include "common/domain_records.h"
+#include "common/result.h"
+
+namespace zchat {
+
+class OrmRepositoryBase : public NonCopyable {
+  public:
+    explicit OrmRepositoryBase(std::shared_ptr<drogon::orm::DbClient> db);
+
+    virtual ~OrmRepositoryBase() = default;
+
+  protected:
+    std::shared_ptr<drogon::orm::DbClient> db_;
+};
+
+std::string FieldString(const drogon::orm::Row &row, const char *name);
+std::int64_t FieldInt64(const drogon::orm::Row &row, const char *name);
+UserRecord ToUserRecord(const drogon::orm::Row &row);
+MessageRecord ToMessageRecord(const drogon::orm::Row &row);
+ChatSessionRecord ToChatSessionRecord(const drogon::orm::Row &row);
+FileRecord ToFileRecord(const drogon::orm::Row &row);
+
+template <typename Func> auto RunDb(Func function) -> decltype(function()) {
+    try {
+        return function();
+    } catch (const drogon::orm::DrogonDbException &error) {
+        using ReturnType = decltype(function());
+        AppError app_error =
+            AppError::WithCode(ErrorCode::kDatabaseError, "数据库操作失败");
+        app_error.detail = error.base().what();
+        return ReturnType::Fail(std::move(app_error));
+    } catch (const std::exception &error) {
+        using ReturnType = decltype(function());
+        AppError app_error =
+            AppError::WithCode(ErrorCode::kUnknown, "服务内部错误");
+        app_error.detail = error.what();
+        return ReturnType::Fail(std::move(app_error));
+    }
+}
+
+} // namespace zchat
+
+#endif // ZCHAT_SERVER_SRC_REPOSITORY_ORM_HELPERS_H_
