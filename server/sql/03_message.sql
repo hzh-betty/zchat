da2fa2be29e2215650968f754e71e6098711c95c
hzh-betty <beidi_99@outlook.com>
2026-06-19 08:54:54 +0800

    chore(sql): 精简数据库 schema，移除冗余字段

--- server/sql/03_message.sql
+++ server/sql/03_message.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS `message` (
   `session_id` varchar(64) NOT NULL,
   `user_id` varchar(64) NOT NULL,
   `message_type` TINYINT UNSIGNED NOT NULL,
-  `create_time` TIMESTAMP NULL,
+  `create_time` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
   `content` MEDIUMTEXT NULL,
   `file_id` varchar(64) NULL,
   `file_name` varchar(128) NULL,
-  `file_size` INT UNSIGNED NULL,
+  `file_size` BIGINT UNSIGNED NULL,
   UNIQUE KEY `message_id_i` (`message_id`),
   KEY `session_id_i` (`session_id`),
   KEY `message_time_i` (`session_id`, `create_time`)
